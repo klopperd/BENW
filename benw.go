36dc2334c6e0bc4f296cd2b75e29c46dae5f3c20
Deon Kloppers <KlopperD@eskom.co.za>
2019-10-29 14:43:37 +0200

    Added sessions

--- benw.go
+++ benw.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/gob"
 	"fmt"
 	"html/template"
 	"log"
@@ -9,8 +10,22 @@ import (
 
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
 	//"github.com/julienschmidt/httprouter"
 )
+
+// User holds a users account information
+type User struct {
+	Username      string
+	Authenticated bool
+}
+
+// store will hold all session data
+var store *sessions.FilesystemStore
+
+// tpl holds all parsed templates
+var tpl *template.Template
 
 // Model of stuff to render a page
 type Model struct {
@@ -23,8 +38,10 @@ type Model struct {
 // the reflect package can only access public/exported struct fields.
 // So try defining
 type pageData struct {
-	Title  string
-	Navbar string
+	Title         string
+	Navbar        string
+	Username      string
+	Authenticated bool
 }
 
 type messData struct {
@@ -43,7 +60,7 @@ type ouData struct {
 	Mc       int
 	Mess     [6]messData
 	ChkMess  bool
-	ChkAudit bool
+	ChkAudit string
 	ShMess   string
 }
 
@@ -55,12 +72,33 @@ var (
 var (
 	server   string
 	port     = 1433
-	user     string
+	benuser  string
 	password string
 	defuser  string
 	defpass  string
 	database = "ben"
 )
+
+func init() {
+	authKeyOne := securecookie.GenerateRandomKey(64)
+	encryptionKeyOne := securecookie.GenerateRandomKey(32)
+
+	store = sessions.NewFilesystemStore(
+		"sessions/",
+		authKeyOne,
+		encryptionKeyOne,
+	)
+
+	store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   60 * 15,
+		HttpOnly: true,
+	}
+
+	gob.Register(User{})
+
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -126,6 +164,24 @@ func summary(db *sql.DB) (int, error) {
 }
 
 func regiontHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "cookie-name")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user := getUser(session)
+
+	if auth := user.Authenticated; !auth {
+		session.AddFlash("You don't have access!")
+		err = session.Save(r, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/forbidden", http.StatusFound)
+		return
+	}
 
 	//Need to do something with this !!!
 	defuser = "elec\\klopperd"
@@ -139,42 +195,42 @@ func regiontHandler(w http.ResponseWriter, r *http.Request) {
 			ou.Name = "ECOU"
 			ou.Desc = "Eastern Cape OU"
 			server = "elnvmsa006\\scadaapps"
-			user = defuser
+			benuser = defuser
 			password = defpass
 		case "kz":
 			ou.Name = "KZNOU"
 			ou.Desc = "KZN OU"
 			server = "mkdvmsa006\\scadaapps"
-			user = defuser
+			benuser = defuser
 			password = defpass
 		case "gt":
 			ou.Name = "GOU"
 			ou.Desc = "Gauteng OU"
 			server = "spnvmsa010\\scadaapps"
-			user = defuser
+			benuser = defuser
 			password = defpass
 		case "mp":
 			ou.Name = "MPOU"
 			ou.Desc = "Mpumalanga OU"
 			server = "wtkvmsa008\\scadaapps"
-			user = defuser
+			benuser = defuser
 			password = defpass
 		case "wc":
 			ou.Name = "WCOU"
 			ou.Desc = "Western Cape OU"
 			server = "blvvmsa014\\scadaapps"
-			user = defuser
+			benuser = defuser
 			password = defpass
 		default:
 			ou.Name = "TEST"
 			ou.Desc = "Test Environment "
 			server = "hwhvmsa004"
-			user = "qaelec\\klopperd"
-			password = "Eskom899#"
+			benuser = "qaelec\\klopperd"
+			password = "Eskom810#"
 		}
 
 		connString := fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s;",
-			server, user, password, database)
+			server, benuser, password, database)
 
 		conn, err := sql.Open("mssql", connString)
 		if err != nil {
@@ -202,8 +258,11 @@ func regiontHandler(w http.ResponseWriter, r *http.Request) {
 		// ou.Mess[1].Message = "Hello"
 		// ou.Mess[2].Message = "twak"
 
-		tmpl := template.Must(template.ParseFiles("region.html", "header.html", "headernav.html"))
-		err = tmpl.Execute(w, ou)
+		fmt.Println("SA Selected on get: ", ou.ChkAudit)
+
+		//tmpl := template.Must(template.ParseFiles("region.html", "header.html", "headernav.html"))
+		//err = tmpl.Execute(w, ou)
+		err = tpl.ExecuteTemplate(w, "region.html", ou)
 		//err := tmpl.Execute(os.Stdout, data)
 		if err != nil {
 			panic(err)
@@ -220,6 +279,11 @@ func regiontHandler(w http.ResponseWriter, r *http.Request) {
 
 		MyShow := r.Form["shOptions"]
 		fmt.Println("SA Selected : ", contains(MyShow, "SA"))
+		if contains(MyShow, "SA") {
+			ou.ChkAudit = "checked"
+		} else {
+			ou.ChkAudit = ""
+		}
 
 		//fmt.Println("shMess = ", ou.ShMess)
 		// if ChkMess {
@@ -273,14 +337,116 @@ func index(w http.ResponseWriter, r *http.Request) {
 	//err := tmpls.ExecuteTemplate(w, "index.html", data)
 	//var tmpls = template.Must(template.ParseFiles("index.html", "headernav.html"))
 
-	pagedata.Title = "Home"
-	tmpl := template.Must(template.ParseFiles("index.html", "header.html", "headernav.html"))
-	err := tmpl.Execute(w, pagedata)
+	
+	//pagedata.Authenticated = false
+	//tmpl := template.Must(template.ParseFiles("index.html", "header.html", "headernav.html"))
+
+	//new session implementation
+	session, err := store.Get(r, "cookie-name")
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user := getUser(session)
+	fmt.Println(user)
+
+	pagedata.Username = user.Username
+	pagedata.Authenticated = user.Authenticated
+	//pagedata.Username = user.Username
+
+	werr := tpl.ExecuteTemplate(w, "index.html", pagedata)
+	if werr != nil {
+		panic(werr)
 	}
 
 	fmt.Println("index executed")
+}
+
+// login authenticates the user
+func login(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "cookie-name")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if r.FormValue("code") != "code" {
+		if r.FormValue("code") == "" {
+			session.AddFlash("Must enter a code")
+		}
+		session.AddFlash("The code was incorrect")
+		err = session.Save(r, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/forbidden", http.StatusFound)
+		return
+	}
+
+	username := r.FormValue("username")
+
+	user := &User{
+		Username:      username,
+		Authenticated: true,
+	}
+
+	session.Values["user"] = user
+
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/region", http.StatusFound)
+}
+
+func forbidden(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "cookie-name")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	flashMessages := session.Flashes()
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tpl.ExecuteTemplate(w, "forbidden.html", flashMessages)
+}
+
+// logout revokes authentication for a user
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "cookie-name")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["user"] = User{}
+	session.Options.MaxAge = -1
+
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+// getUser returns a user from session s
+// on error returns an empty user
+func getUser(s *sessions.Session) User {
+	val := s.Values["user"]
+	var user = User{}
+	user, ok := val.(User)
+	if !ok {
+		return User{Authenticated: false}
+	}
+	return user
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
@@ -294,6 +460,9 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", index)
+	router.HandleFunc("/login", login)
+	router.HandleFunc("/forbidden", forbidden)
+	router.HandleFunc("/logout", logout)
 	router.HandleFunc("/about", about)
 	router.HandleFunc("/test", testHandler)
 	router.HandleFunc("/region", regiontHandler)
